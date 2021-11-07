300c2f28489271da521b3625c68743b545af3a8f
Van4kk <65396868+Van4kk@users.noreply.github.com>
2021-11-07 10:40:05 +0200

    Russian Music BOT for Discord

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('OTA2NTQ5NzA0MDAxNzE2MjY1.YYaQNA.yrxS3D-DhPOP_F-X-CVIqUyR8fI')
+
+
