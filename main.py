228f50389bd84ec853d6d92d6322b68b1e19019f
Van4kk <65396868+Van4kk@users.noreply.github.com>
2022-08-22 21:26:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
-client.run('OTA2NTQ5NzA0MDAxNzE2MjY1.YYaQNA.vyM7pjYSgXiaBkXOWNJK-p9_wow')
+client.run('TOKEN')
 
 
